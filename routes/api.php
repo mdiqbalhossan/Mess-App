0bd50bc67ed49b9c390e572b79dfb5030de88b9b
mdiqbalhossan <jmiqbal7265@gmail.com>
2023-12-29 02:43:36 +0600

    =Major Changes

--- routes/api.php
+++ routes/api.php
@@ -10,37 +10,37 @@ use App\Http\Controllers\Api\InformationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.email');
-
-Route::post('/reset-password', [NewPasswordController::class, 'store'])
-    ->middleware('guest')
-    ->name('password.store');
-
-Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-    ->middleware(['auth', 'signed', 'throttle:6,1'])
-    ->name('verification.verify');
-
-Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth', 'throttle:6,1'])
-    ->name('verification.send');
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
-
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::post('/register', [RegisteredUserController::class, 'store'])
+//    ->middleware('guest')
+//    ->name('register');
+//
+//Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+//    ->middleware('guest')
+//    ->name('login');
+//
+//Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+//    ->middleware('guest')
+//    ->name('password.email');
+//
+//Route::post('/reset-password', [NewPasswordController::class, 'store'])
+//    ->middleware('guest')
+//    ->name('password.store');
+//
+//Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+//    ->middleware(['auth', 'signed', 'throttle:6,1'])
+//    ->name('verification.verify');
+//
+//Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+//    ->middleware(['auth', 'throttle:6,1'])
+//    ->name('verification.send');
+//
+//Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+//    ->middleware('auth')
+//    ->name('logout');
+//
+//Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::get('users', [InformationController::class, 'alluser']);
 Route::get('/details/{user}', [InformationController::class, 'details']);
